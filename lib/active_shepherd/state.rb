bf7e955ba7b629c140f19ec441eadbbd7ebd164a
ntl <nathanladd+github@gmail.com>
2013-03-10 12:09:47 -0500

    Extract add state from root model method

--- lib/active_shepherd/state.rb
+++ lib/active_shepherd/state.rb
@@ -7,17 +7,7 @@ class ActiveShepherd::State
   end
 
   def state
-    default_attributes = aggregate.default_attributes
-
-    aggregate.model.attributes_before_type_cast.each do |attribute_name, value|
-      next if aggregate.excluded_attributes.include?(attribute_name)
-
-      value = aggregate.serialize_value(attribute_name, value)
-
-      unless value == default_attributes[attribute_name]
-        hash[attribute_name.to_sym] = value
-      end
-    end
+    add_state_from_root_model
 
     aggregate.traversable_associations.each do |name, association_reflection|
       serialized = get_via_association(association_reflection)
@@ -28,6 +18,18 @@ class ActiveShepherd::State
   end
 
 private
+
+  def add_state_from_root_model
+    aggregate.model.attributes_before_type_cast.each do |attribute_name, value|
+      next if aggregate.excluded_attributes.include?(attribute_name)
+
+      value = aggregate.serialize_value(attribute_name, value)
+
+      unless value == aggregate.default_attributes[attribute_name]
+        hash[attribute_name.to_sym] = value
+      end
+    end
+  end
 
   def get_via_association(association_reflection)
     foreign_key_to_self = association_reflection.foreign_key
