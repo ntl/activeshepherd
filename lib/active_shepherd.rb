ee607e7312448a96f9bc8c355991519be2ee53cf
ntl <nathanladd+github@gmail.com>
2013-03-10 18:57:31 -0500

    Move Traversal to its own file

--- lib/active_shepherd.rb
+++ lib/active_shepherd.rb
@@ -3,9 +3,10 @@ module ActiveShepherd ; end
 require 'active_shepherd/active_record_shim'
 require 'active_shepherd/aggregate'
 require 'active_shepherd/aggregate_root'
+require 'active_shepherd/deep_reverse_changes'
 require 'active_shepherd/method'
 require 'active_shepherd/methods'
-require 'active_shepherd/deep_reverse_changes'
+require 'active_shepherd/traversal'
 require 'active_shepherd/version'
 
 module ActiveShepherd
--- lib/active_shepherd/method.rb
+++ lib/active_shepherd/method.rb
@@ -29,43 +29,6 @@ class ActiveShepherd::Method
   end
 end
 
-module ActiveShepherd
-  class Traversal
-    attr_reader :attributes, :associations, :visitor
-
-    def initialize(visitor, params = {})
-      @associations = params[:associations]
-      @attributes   = params[:attributes]
-      @visitor      = visitor
-    end
-
-    def traverse
-      attributes.each do |attribute_name, object|
-        visit :handle_attribute, attribute_name, object
-      end
-
-      # XXX: no need for name here
-      associations.each do |name, (reflection, object)|
-        if reflection.macro == :has_many
-          visit :handle_has_many_association, reflection, object
-        elsif reflection.macro == :has_one
-          visit :handle_has_one_association, reflection, object
-        end
-      end
-    end
-
-    def visit(method_name, arg1, arg2)
-      if visitor.respond_to? method_name
-        if visitor.method(method_name).arity == 2
-          visitor.public_send method_name, arg1, arg2
-        else
-          visitor.public_send method_name, arg1, *Array.wrap(arg2)
-        end
-      end
-    end
-  end
-end
-
 class ActiveShepherd::QueryMethod < ActiveShepherd::Method
   attr_reader :query
 
