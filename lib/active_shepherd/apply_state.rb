9921d3cc3b374b02c628b8d6300c8a816ecb4212
ntl <nathanladd+github@gmail.com>
2013-03-10 14:16:46 -0500

    Start creating base subclass for commands

--- lib/active_shepherd/apply_state.rb
+++ lib/active_shepherd/apply_state.rb
@@ -1,11 +1,38 @@
-class ActiveShepherd::ApplyState
+class ActiveShepherd::StateMethod
   attr_reader :aggregate, :hash
 
   def initialize(aggregate, hash)
     @aggregate = aggregate
-    @hash      = hash.dup
+    @hash      = hash
   end
 
+private
+
+  def associations
+    split_hash[:associations]
+  end
+
+  def attributes
+    split_hash[:attributes]
+  end
+
+  def split_hash
+    @split_hash ||= begin
+      split_hash = { associations: {}, attributes: {} }
+      hash.each_with_object(split_hash) do |(key, value), by_key|
+        traversable_association = aggregate.traversable_associations[key]
+        if traversable_association.present?
+          by_key[:associations][key] = [traversable_association, value]
+        elsif aggregate.untraversable_association_names.include? key
+        else
+          by_key[:attributes][key] = value
+        end
+      end
+    end
+  end
+end
+
+class ActiveShepherd::ApplyState < ActiveShepherd::StateMethod
   def apply_state
     mark_all_associated_objects_for_destruction
     apply_default_state_to_root_model
@@ -39,35 +66,12 @@ class ActiveShepherd::ApplyState
   end
 
   def apply_state_to_associations
-    associations.each do |association_name, (association_reflection, state)|
+    associations.values.each do |association_reflection, state|
       apply_state_to_association association_reflection, state
     end
   end
 
-  def associations
-    split_hash[:associations]
-  end
-
-  def attributes
-    split_hash[:attributes]
-  end
-
 private
-
-  def split_hash
-    @split_hash ||= begin
-      split_hash = { associations: {}, attributes: {} }
-      hash.each_with_object(split_hash) do |(key, value), by_key|
-        traversable_association = aggregate.traversable_associations[key]
-        if traversable_association.present?
-          by_key[:associations][key] = [traversable_association, value]
-        elsif aggregate.untraversable_association_names.include? key
-        else
-          by_key[:attributes][key] = value
-        end
-      end
-    end
-  end
 
   def mark_all_associated_objects_for_destruction
     aggregate.traversable_associations.each do |name, association_reflection|
