03820391fb303539b079c2e477c0b4df4938ff6f
ntl <nathanladd+github@gmail.com>
2013-03-10 12:08:42 -0500

    [WHITESPACE]

--- lib/active_shepherd/state.rb
+++ lib/active_shepherd/state.rb
@@ -9,20 +9,20 @@ class ActiveShepherd::State
   def state
     default_attributes = aggregate.default_attributes
 
-      aggregate.model.attributes_before_type_cast.each do |attribute_name, value|
-        next if aggregate.excluded_attributes.include?(attribute_name)
+    aggregate.model.attributes_before_type_cast.each do |attribute_name, value|
+      next if aggregate.excluded_attributes.include?(attribute_name)
 
-        value = aggregate.serialize_value(attribute_name, value)
+      value = aggregate.serialize_value(attribute_name, value)
 
-        unless value == default_attributes[attribute_name]
-          hash[attribute_name.to_sym] = value
-        end
+      unless value == default_attributes[attribute_name]
+        hash[attribute_name.to_sym] = value
       end
+    end
 
-      aggregate.traversable_associations.each do |name, association_reflection|
-        serialized = get_via_association(association_reflection)
-        hash[name.to_sym] = serialized unless serialized.blank?
-      end
+    aggregate.traversable_associations.each do |name, association_reflection|
+      serialized = get_via_association(association_reflection)
+      hash[name.to_sym] = serialized unless serialized.blank?
+    end
 
     hash
   end
