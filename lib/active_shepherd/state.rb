64e9e871c67179224bd65246dceebd9a8ee7bc4a
ntl <nathanladd+github@gmail.com>
2013-03-10 12:04:29 -0500

    Make State#get_via_association a private

--- lib/active_shepherd/state.rb
+++ lib/active_shepherd/state.rb
@@ -6,23 +6,6 @@ class ActiveShepherd::State
     @hash = {}
   end
 
-  def get_via_association(association_reflection)
-    foreign_key_to_self = association_reflection.foreign_key
-    model_or_collection_of_models = aggregate.model.send(association_reflection.name)
-
-    if model_or_collection_of_models.nil?
-      # noop
-    elsif association_reflection.macro == :belongs_to
-      # noop
-    elsif association_reflection.macro == :has_many
-      model_or_collection_of_models.to_a.select(&:present?).map do |associated_model|
-        ::ActiveShepherd::Aggregate.new(associated_model, foreign_key_to_self).state
-      end
-    else
-      ::ActiveShepherd::Aggregate.new(model_or_collection_of_models, foreign_key_to_self).state
-    end
-  end
-
   def state
     default_attributes = aggregate.model.class.new.attributes
 
@@ -41,6 +24,25 @@ class ActiveShepherd::State
         serialized = get_via_association(association_reflection)
         hash[name.to_sym] = serialized unless serialized.blank?
       end
+    end
+  end
+
+private
+
+  def get_via_association(association_reflection)
+    foreign_key_to_self = association_reflection.foreign_key
+    model_or_collection_of_models = aggregate.model.send(association_reflection.name)
+
+    if model_or_collection_of_models.nil?
+      # noop
+    elsif association_reflection.macro == :belongs_to
+      # noop
+    elsif association_reflection.macro == :has_many
+      model_or_collection_of_models.to_a.select(&:present?).map do |associated_model|
+        ::ActiveShepherd::Aggregate.new(associated_model, foreign_key_to_self).state
+      end
+    else
+      ::ActiveShepherd::Aggregate.new(model_or_collection_of_models, foreign_key_to_self).state
     end
   end
 end
